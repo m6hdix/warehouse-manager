2d4e6a5820979ed3622088d37f57a87eee7f3b08
Mehdi Gholam Hosseini <96904286+m6hdix@users.noreply.github.com>
2022-06-18 10:17:36 +0430

    Firast Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './dist/**/*.{html,js}',
+        './components/**/*.{html,js}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
+}
